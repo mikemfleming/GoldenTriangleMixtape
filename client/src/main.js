743b5f0d9bb24d46e0397f29ccedb4b97c3e72bf
Michael Fleming <flemingatx@gmail.com>
2017-05-09 21:16:31 -0500

    begins iframe

--- client/src/main.js
+++ client/src/main.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import InputBar from './inputBar.js'
+import MediaList from './mediaList.jsx'
 
 class App extends Component {
   constructor (props) {
     super(props)
 
-    this.state= {
+    this.state = {
+      submission: ''
     }; 
 
+    this.addToCatalog = this.addToCatalog.bind(this)
+
+  }
+
+  handleChange(e) {
+    this.setState({submission: e.target.value})
+  }
+
+  addToCatalog() {
+    alert(this.state.submission)
   }
 
   render () {
     return (
       <div>
-        <h1>Oh shit waddup</h1>
+        <InputBar add={this.addToCatalog.bind(this)} handleChange={this.handleChange.bind(this)}/>
+        <MediaList media={this.state.submission} />
       </div>
     );
   }
---
+++ client/src/mediaList.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class MediaList extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+    }; 
+
+  }
+
+  render () {
+    return (
+      <div>
+        {this.props.media}
+      </div>
+    );
+  }
+
+}
