230c2070a4eb04341673a83c35e994138fb21fc8
Michael Fleming <flemingatx@gmail.com>
2017-05-10 20:48:55 -0500

    testing

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use('/app-bundle.js',
 );
 
 app.post('/api/submit', (req, res) => {
-  console.log(req.body)
+  console.log('*****************************************', req.body)
   res.end()
 })
 
