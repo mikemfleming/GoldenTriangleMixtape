fe15592e42c8916ed823b11b698fb1b7613442b2
Michael Fleming <flemingatx@gmail.com>
2017-05-16 18:52:45 -0500

    emits on submit

--- index.js
+++ index.js
@@ -22,14 +22,19 @@ app.use('/app-bundle.js',
   })
 );
 
-app.post('/api/submit', (req, res) => {
-  console.log('$$$$$$$$$$', req.body)
-  res.end()
-})
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
+
+app.post('/api/submit', (req, res) => {
+  io.emit('submission', req.body)
+  console.log('$$$$$$$$$$', req.body)
+  res.end()
+})
 
 var port = process.env.PORT || 8080;
 http.listen(port);
